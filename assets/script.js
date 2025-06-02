6de03066890e9640202bb374651a0aec74a8811e
A.B.L <alex.ben-larbi@hotmail.fr>
2025-06-02 11:42:38 +0200

    02/06/2025 11h42 : Ajout du Javascript pour le timer, la fonction pour qu'il fonctionne + modification de "script.js" pour ajouter la nouvelle fonction + nettoyage des commentaires.

--- assets/script.js
+++ assets/script.js
@@ -27,10 +27,10 @@ const txt = document.querySelector('#banner p')
 
 /** Fabrication des bullets point. Pour faire ça le plus proprement possible :
  * On crée la variable "dot" qui va contenir notre dot.
- * On crée "dotsContainer" qui renvoie au div déjà existant dans le code HTML
- * On insère dans le HTML, dans "dotsContainer" nos dots (autant qu'il y a d'élément dans le tableau "slides")
- * On place la classe "dot_selected" au premier div qui se trouve dans "dotsContainer"
- * On crée la variable "allDots" qui va fonctionner comme un mini-tableau */
+ * On crée "dotsContainer" qui renvoie au div déjà existant dans le code HTML.
+ * On insère dans le HTML, dans "dotsContainer" nos dots (autant qu'il y a d'élément dans le tableau "slides").
+ * On place la classe "dot_selected" au premier div qui se trouve dans "dotsContainer".
+ * On crée la variable "allDots" qui va fonctionner comme un mini-tableau. */
 
 let dot = `<div class="dot"></div>`
 let dotsContainer = document.querySelector('.dots')
@@ -41,13 +41,13 @@ const allDots = document.querySelectorAll('.dot')
 /** Création de la fonction pour passer à la slide suivant :
  * On retire la classe "dot_selected" du dot actuel (représenté par index).
  * On augmente index de 1, dans l'idée on passe à la case suivante.
- * Si index dépasse nb_slides-1 (ici 3, la première case est toujours 0), alors
+ * Si index dépasse nb_slides-1 (ici 3, la première case est toujours 0), alors...
  * On retourne à 0, donc à la première slide.
  * 
- * On modifie ensuite le chemin de l'image. Spécifiquement slides.images
+ * On modifie ensuite le chemin de l'image. Spécifiquement slides.images.
  * Pareil avec le texte.
- * On place "dot_selected sur le nouveau dot actuel"
- */
+ * On place "dot_selected sur le nouveau dot actuel".
+ * Voir "timer.js" pour resetautonext. */
 
 function nextSlide() {
 	allDots[index].classList.remove('dot_selected');
@@ -58,6 +58,7 @@ function nextSlide() {
 	img.src = `./assets/images/slideshow/${slides[index].image}`;
 	txt.innerHTML = slides[index].tagLine;
 	allDots[index].classList.add('dot_selected');
+	resetautonext();
 	console.log('--> Nouvelle Slide !')
 }
 
@@ -65,11 +66,10 @@ function nextSlide() {
  * Création de la fonction pour passer à la slide précédente :
  * Fonctionnellement la même chose que la fonction précédente, avec quelques changements.
  * On réduit index de 1.
- * Si index est en dessous de 0, alors
+ * Si index est en dessous de 0, alors...
  * On le place à nb_slides - 1.
  * 
- * Même chose que la fonction précédente.
- */
+ * Même chose que la fonction précédente. */
 
 function backSlide() {
 	allDots[index].classList.remove('dot_selected');
@@ -80,6 +80,7 @@ function backSlide() {
 	img.src = `./assets/images/slideshow/${slides[index].image}`;
 	txt.innerHTML = slides[index].tagLine;
 	allDots[index].classList.add('dot_selected');
+	resetautonext();
 	console.log('--> Retour arrière !')
 }
 
--- assets/timer.js
+++ assets/timer.js
@@ -13,3 +13,27 @@ document.querySelector('#banner').appendChild(timerBarContainer)
     timerBar.style.height = '100%';
     timerBar.style.backgroundColor = '#FF0000';
 }
+
+let autonext = setInterval(nextSlide, 10000)
+
+function resetautonext() {
+    /** Le but de cette fonction est de faire 3 choses :
+     * 1) Remettre à 0 'setInterval. Pour cela on le supprime, puis on le remet en place.
+     * 2) On met en place le CSS pour la barre de progression à 0.
+     * 3) Mettre en place le CSS pour que la barre de progression se remplisse.
+     * Entre le 2 et le 3, on force le navigateur à mettre à jour la page (parce qu'il est un peu fainéant). */
+
+    clearInterval(autonext);
+    autonext = setInterval(nextSlide, 10000);
+
+    timerBar.style.transition = 'none';
+    timerBar.style.width = '0%';
+
+    void timerBar.offsetWidth;
+
+    timerBar.style.transition = 'width 10s linear';
+    timerBar.style.width = '100%';
+}
+
+// Pour activer la barre de progression, on utilise une fois la fonction à l'initialisation de la page.
+resetautonext();
