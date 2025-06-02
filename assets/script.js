5130846b6af34362afb9cd3b77ca8ac209688d21
A.B.L <alex.ben-larbi@hotmail.fr>
2025-06-02 10:46:25 +0200

    02/06/2025 10h45 : Création des bullet point

--- assets/script.js
+++ assets/script.js
@@ -24,3 +24,16 @@ const back = document.querySelector('.arrow_left')
 const next = document.querySelector('.arrow_right')
 const img = document.querySelector('.banner-img')
 const txt = document.querySelector('#banner p')
+
+/** Fabrication des bullets point. Pour faire ça le plus proprement possible :
+ * On crée la variable "dot" qui va contenir notre dot.
+ * On crée "dotsContainer" qui renvoie au div déjà existant dans le code HTML
+ * On insère dans le HTML, dans "dotsContainer" nos dots (autant qu'il y a d'élément dans le tableau "slides")
+ * On place la classe "dot_selected" au premier div qui se trouve dans "dotsContainer"
+ * On crée la variable "allDots" qui va fonctionner comme un mini-tableau */
+
+let dot = `<div class="dot"></div>`
+let dotsContainer = document.querySelector('.dots')
+dotsContainer.innerHTML=`${dot.repeat(slides.length)}`
+dotsContainer.firstChild.classList.add('dot_selected')
+const allDots = document.querySelectorAll('.dot')
